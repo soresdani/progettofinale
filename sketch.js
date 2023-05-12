8d780a43ee870c99a157040b4cc3e6e904d0a811
danielesoresina <daniele.soresina@marconicloud.it>
2023-05-12 13:12:09 +0200

    change  background color

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@
 
 **/
 
-function preload() {
+/*function preload() {
   treeModel = loadAsset("albero.glb");
 }
 
@@ -29,4 +29,45 @@ function setup() {
 function draw() {
   // 🔍 Never create objects here, but don't forget to have it in your code:
   // leave empty for the moment
+}*/
+
+function setup() {
+  createCanvas3D(windowWidth, windowHeight);
+  /*createCanvas3D(600, 400);
+  /*background3D("#00A0F0");*/
+  /*enviroment(NEON);*/
+   environment(STARS);
+  
+  var quercia = beginGroup();
+  diffuse("#76c893");
+  sphere(0, 2.7, 0, 1.6);
+  capsule(0, 2.7, 0, 1.2, 1);
+  sphere(1, 3.2, 0, 1.4);
+  diffuse("#184e77");
+  align(TOP);
+  cylinder(0, 0, 0, 0.3, 2.4);
+  endGroup();
+  
+  var lato = 70;
+  var numeroAlberi = 90;
+  for (var i=0; i<numeroAlberi; i++)
+    {
+      var x = random(-lato/2, lato/2);
+      var y = 0;
+      var z = random(-lato/2, lato/2);
+
+      clone(quercia, x, y, z);
+      
+      var nuovoAlbero = clone(quercia, x, y, z);
+      nuovoAlbero.setScale(random(0.5, 1.6));
+    }
+ align(BOTTOM);
+ diffuse("#34a0a4");
+ box(0, 0, 0, lato, 2, lato, 2);
+ pushFX(BLOOM, 0.18);
+ 
+}
+
+function draw() {
+  
 }
